5e8fab9bb7a8a3524b38e9e0cff28ab36bb83656
aswma317 <aswma317@student.liu.se>
2022-09-17 01:27:40 +0200

    1. Included the 2nd plot in plot() function.

--- R/LinReg_Class.R
+++ R/LinReg_Class.R
@@ -11,6 +11,7 @@
 #' @return
 #' @export
 #' @import ggplot2
+#' @import gridExtra
 #'
 #' @examples
 linregClass <- setRefClass('linregClass',
@@ -34,15 +35,29 @@ linregClass <- setRefClass('linregClass',
                           return(est_beta)
                         },
                         plot = function(){
-                          df <- data.frame(Fitted_Values = y_pred,
+                          df1 <- data.frame(Fitted_Values = y_pred,
                                            Residuals = resid_e)
-                          ggplot(df,
+                          plot_1 <- ggplot(df1,
                                  aes(x=Fitted_Values, y=Residuals, group = 1)) +
-                            geom_point(size=2.5, shape = 1)+
+                            geom_point(size=2.5, shape = 1) +
+                            ggtitle('Residuals vs Fitted') +
+                            theme(plot.title = element_text(hjust = 0.5)) +
                             geom_smooth(method = "lm",
                                         linetype = "dotted",
                                         se = FALSE)+
                             stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
+                          
+                          df2 <- data.frame(Fitted_Values = y_pred,
+                                            Standardized_Residuals = sqrt(resid_var_e))
+                          plot_2 <- ggplot(df2,
+                                           aes(x=Fitted_Values, y=Standardized_Residuals, group = 1)) +
+                            geom_point(size=2.5, shape = 1) +
+                            ggtitle('Scale−Location') +
+                            theme(plot.title = element_text(hjust = 0.5)) 
+                            # geom_smooth(method = "lm",
+                            #             linetype = "dotted",
+                            #             se = FALSE)+
+                            # stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
+                          grid.arrange(plot_1, plot_2, nrow = 1)
                         }
                       ))
-
