79f453803f32c2939021bc133dd08d63dd96aaf8
VarunRavi95 <varsi146@student.liu.se>
2022-09-16 22:34:38 +0200

    Print() method added

--- R/LinReg_Class.R
+++ R/LinReg_Class.R
@@ -7,4 +7,12 @@ linregClass <- setRefClass('linregClass',
                         resid_var_e = 'matrix',
                         var_est_beta = 'vector',
                         t_val_beta = 'matrix'
+                      ),
+                      methods = list(
+                        pred = function(){
+                          return(y_pred)
+                        },
+                        print = function(){
+                          return(list("Coefficients:" = est_beta))
+                        }
                       ))
