25d03723258ba59a2f2b125767c3246471391780
aswma317 <aswma317@student.liu.se>
2022-09-17 00:22:13 +0200

    1. Namespace contains the exports and imports 2. Included coef and plot function 3. Included man folders

--- R/LinReg_Class.R
+++ R/LinReg_Class.R
@@ -1,3 +1,18 @@
+#' Title
+#'
+#' @field est_beta vector. 
+#' @field y_pred matrix. 
+#' @field resid_e matrix. 
+#' @field deg_freed numeric. 
+#' @field resid_var_e matrix. 
+#' @field var_est_beta vector. 
+#' @field t_val_beta matrix. 
+#'
+#' @return
+#' @export
+#' @import ggplot2
+#'
+#' @examples
 linregClass <- setRefClass('linregClass',
                       fields = list(
                         est_beta = 'vector',
@@ -14,5 +29,20 @@ linregClass <- setRefClass('linregClass',
                         },
                         print = function(){
                           return(list("Coefficients:" = est_beta))
+                        },
+                        coef = function(){
+                          return(est_beta)
+                        },
+                        plot = function(){
+                          df <- data.frame(Fitted_Values = y_pred,
+                                           Residuals = resid_e)
+                          ggplot(df,
+                                 aes(x=Fitted_Values, y=Residuals, group = 1)) +
+                            geom_point(size=2.5, shape = 1)+
+                            geom_smooth(method = "lm",
+                                        linetype = "dotted",
+                                        se = FALSE)+
+                            stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
                         }
                       ))
+
