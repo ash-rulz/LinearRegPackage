e6ef5cb918d2c12b9bbd33e0758d0ccc521c41e3
VarunRavi95 <varsi146@student.liu.se>
2022-09-17 19:36:45 +0200

    Added 'xlab' and 'ylab' to plot 1

--- R/LinReg_Class.R
+++ R/LinReg_Class.R
@@ -1,12 +1,12 @@
 #' Title
 #'
-#' @field est_beta vector. 
-#' @field y_pred matrix. 
-#' @field resid_e matrix. 
-#' @field deg_freed numeric. 
-#' @field resid_var_e matrix. 
-#' @field var_est_beta vector. 
-#' @field t_val_beta matrix. 
+#' @field est_beta vector.
+#' @field y_pred matrix.
+#' @field resid_e matrix.
+#' @field deg_freed numeric.
+#' @field resid_var_e matrix.
+#' @field var_est_beta vector.
+#' @field t_val_beta matrix.
 #'
 #' @return
 #' @export
@@ -22,14 +22,18 @@ linregClass <- setRefClass('linregClass',
                         deg_freed = 'numeric',
                         resid_var_e = 'matrix',
                         var_est_beta = 'vector',
-                        t_val_beta = 'matrix'
+                        t_val_beta = 'matrix',
+                        formula = 'formula',
+                        data = 'data.frame'
                       ),
                       methods = list(
                         pred = function(){
                           return(y_pred)
                         },
                         print = function(){
-                          return(list("Coefficients:" = est_beta))
+                          cat("Coefficients:\n")
+                          print.default(format(est_beta, digits = 2),
+                                        print.gap = 2L, quote = FALSE)
                         },
                         coef = function(){
                           return(est_beta)
@@ -41,23 +45,29 @@ linregClass <- setRefClass('linregClass',
                                  aes(x=Fitted_Values, y=Residuals, group = 1)) +
                             geom_point(size=2.5, shape = 1) +
                             ggtitle('Residuals vs Fitted') +
+                            xlab(paste('Fitted Values \n','linreg(', format(formula),')'))+
+                            ylab('Residuals')+
                             theme(plot.title = element_text(hjust = 0.5)) +
                             geom_smooth(method = "lm",
                                         linetype = "dotted",
                                         se = FALSE)+
                             stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
-                          
+
                           df2 <- data.frame(Fitted_Values = y_pred,
                                             Standardized_Residuals = sqrt(resid_var_e))
                           plot_2 <- ggplot(df2,
                                            aes(x=Fitted_Values, y=Standardized_Residuals, group = 1)) +
                             geom_point(size=2.5, shape = 1) +
                             ggtitle('Scale−Location') +
-                            theme(plot.title = element_text(hjust = 0.5)) 
+                            theme(plot.title = element_text(hjust = 0.5))
                             # geom_smooth(method = "lm",
                             #             linetype = "dotted",
                             #             se = FALSE)+
                             # stat_summary(fun.y=median, colour="red", geom="line", aes(group = 1))
                           grid.arrange(plot_1, plot_2, nrow = 1)
+                        },
+                        resid = function(){
+                          return(c(resid_e))
                         }
                       ))
+
--- R/LinearReg.R
+++ R/LinearReg.R
@@ -1,4 +1,6 @@
 linreg <- function(formula, data){
+  formula_f <- formula
+  data_f <- data
   #Create the design matrix X.
   #The first attribute is the independent variable in formula.
   X <- model.matrix(formula, data)
@@ -26,16 +28,29 @@ linreg <- function(formula, data){
                                 deg_freed = deg_freed_f,
                                 resid_var_e = resid_var_e_f,
                                 var_est_beta = var_est_beta_f,
-                                t_val_beta = t_val_f
+                                t_val_beta = t_val_f,
+                                formula = formula_f,
+                                data = data_f
                                 )
   return(linreg_obj)
 }
 
 linreg_obj <- linreg(Petal.Length~Species, data = iris)
 linreg_obj$t_val_beta
+linreg_obj$pred()
+linreg_obj$print()
+linreg_obj$plot()
+linreg_obj$resid()
 
-
+iris
 X <- model.matrix(Petal.Length~Species, iris)
+X
 #Get the dependent matrix y
 y <- as.matrix(iris[,all.vars(Petal.Length~Species)[1]])
+y
 
+#Calculating the equations using ordinary least squares
+est_beta_f <- solve(t(X)%*%X)%*%(t(X)%*% y) #Regressions coefficient
+y_pred_f <- X %*% est_beta_f #Fitted Values
+resid_e_f <- y - y_pred_f #Residuals
+resid_e_f
